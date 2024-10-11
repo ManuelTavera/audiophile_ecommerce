c282ca792ab71573733346317cebd0e90f598ff6
ManuelTavera <manuel@padsquad.com>
2024-10-10 21:23:09 -0400

    Finish home page section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 import {
   AboutWrapper,
@@ -8,8 +9,32 @@ import {
   Highlight,
   AboutInformation,
   ImageWrapper,
+  FooterSection,
+  FooterWrapper,
+  LogoWrapper,
+  Logo,
+  FooterNavList,
+  FooterNavItem,
+  FooterNavLink,
+  FooterRow,
+  FooterAboutDescription,
+  FooterSocialList,
+  FooterSocialItem,
+  FooterCopyrightText,
+  FooterAboutRow,
+  FooterRowSpecial,
+  Line
 } from "./Footer.style";
-import { BREAKPOINTS } from "@/constant";
+import { BREAKPOINTS, NAVIGATION_LINKS } from "@/constant";
+import FacebookIcon from "@/public/icon/icon-facebook.svg";
+import TwitterIcon from "@/public/icon/icon-twitter.svg";
+import InstagramIcon from "@/public/icon/icon-instagram.svg";
+
+const SOCIAL_LINKS = [
+  { Icon: FacebookIcon, href: "/", id: "Facebook" },
+  { Icon: TwitterIcon, href: "/", id: "Twitter" },
+  { Icon: InstagramIcon, href: "/", id: "Instagram" },
+];
 
 function Footer() {
   return (
@@ -42,6 +67,60 @@ function Footer() {
           </picture>
         </ImageWrapper>
       </AboutWrapper>
+      <FooterSection>
+        <FooterWrapper>
+          <Line />
+          <FooterRow>
+            <LogoWrapper>
+              <Link href={"/"}>
+                <Logo src="/icon/logo.svg" alt="Logo" />
+              </Link>
+            </LogoWrapper>
+            <FooterNavList>
+              {NAVIGATION_LINKS.map(({ name, href }) => (
+                <FooterNavItem key={name}>
+                  <Link href={href} passHref legacyBehavior>
+                    <FooterNavLink variant="subtitle" forwardedAs={"a"}>
+                      {name}
+                    </FooterNavLink>
+                  </Link>
+                </FooterNavItem>
+              ))}
+            </FooterNavList>
+          </FooterRow>
+          <FooterAboutRow>
+            <FooterAboutDescription variant="body">
+              Audiophile is an all in one stop to fulfill your audio needs.
+              We're a small team of music lovers and sound specialists who are
+              devoted to helping you get the most out of personal audio. Come
+              and visit our demo facility - we’re open 7 days a week.
+            </FooterAboutDescription>
+            <FooterSocialList>
+              {SOCIAL_LINKS.map(({ Icon, href, id }) => (
+                <FooterSocialItem key={id}>
+                  <Link href={href}>
+                    <Icon />
+                  </Link>
+                </FooterSocialItem>
+              ))}
+            </FooterSocialList>
+          </FooterAboutRow>
+          <FooterRowSpecial>
+            <FooterCopyrightText variant="body">
+              Copyright 2021. All Rights Reserved
+            </FooterCopyrightText>
+            <FooterSocialList>
+              {SOCIAL_LINKS.map(({ Icon, href, id }) => (
+                <FooterSocialItem key={id}>
+                  <Link href={href}>
+                    <Icon />
+                  </Link>
+                </FooterSocialItem>
+              ))}
+            </FooterSocialList>
+          </FooterRowSpecial>
+        </FooterWrapper>
+      </FooterSection>
     </>
   );
 }
--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -7,6 +7,7 @@ import { COLORS, MEDIA_QUERIES } from "@/constant";
 export const AboutWrapper = styled(MainWrapper)`
   display: flex;
   gap: 32px;
+  margin-bottom: 200px;
 
   ${MEDIA_QUERIES["tablet"]} {
     flex-direction: column-reverse;
@@ -68,4 +69,170 @@ export const ImageWrapper = styled.div`
   flex: 1;
   overflow: hidden;
   border-radius: 8px;
+`;
+
+export const FooterSection = styled.footer`
+  background-color: ${COLORS["black"]};
+`;
+
+export const FooterWrapper = styled(MainWrapper)`
+  position: relative;
+  padding-top: 75px;
+  padding-bottom: 48px;
+
+  ${MEDIA_QUERIES["tablet"]} {
+    padding-top: 60px;
+    padding-bottom: 46px;
+  }
+
+  ${MEDIA_QUERIES["mobile"]} {
+    padding-top: 52px;
+    padding-bottom: 38px;
+  }
+`;
+
+export const LogoWrapper = styled.div``;
+
+export const FooterRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 36px;
+
+  ${MEDIA_QUERIES["tablet"]} {
+    flex-direction: column;
+    gap: 32px;
+  }
+
+  ${MEDIA_QUERIES["mobile"]} {
+    gap: 48px;
+    margin-bottom: 48px;
+    align-items: center;
+  }
+`;
+
+export const Logo = styled.img``;
+
+export const FooterNavList = styled.ul`
+  list-style-type: none;
+  display: flex;
+  gap: 34px;
+  padding: 0;
+  margin: 0;
+
+  ${MEDIA_QUERIES["mobile"]} {
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+  }
+`;
+
+export const FooterNavLink = styled(Typography)`
+  --letterSpacing: ${2 / 16}rem;
+  --color: ${COLORS["white"]};
+
+  text-decoration: none;
+`;
+
+export const FooterNavItem = styled.li`
+  &:hover ${FooterNavLink} {
+    --color: ${COLORS["orange"]};
+  }
+`;
+
+export const FooterAboutDescription = styled(Typography)`
+  --color: color-mix(in hsl, ${COLORS["white"]}, transparent);
+  flex: 0 1 540px;
+
+  ${MEDIA_QUERIES["tablet"]} {
+    flex: 1;
+  }
+
+  ${MEDIA_QUERIES["mobile"]} {
+    text-align: center;
+  }
+`;
+
+export const FooterCopyrightText = styled(Typography)`
+  --color: color-mix(in hsl, ${COLORS["white"]}, transparent);
+`;
+
+export const FooterSocialList = styled.ul`
+  list-style-type: none;
+  display: flex;
+  gap: 16px;
+  padding: 0;
+`;
+
+export const FooterAboutRow = styled(FooterRow)`
+  align-items: last baseline;
+
+  ${MEDIA_QUERIES["tablet"]} {
+    & ${FooterSocialList} {
+      display: none;
+    }
+  }
+
+  ${MEDIA_QUERIES["mobile"]} {
+    align-items: center;
+    gap: 48px;
+    margin-bottom: 48px;
+  }
+`;
+
+export const FooterSocialItem = styled.li`
+  width: 24px;
+  height: 24px;
+  color: ${COLORS["white"]};
+  cursor: pointer;
+
+  &:hover svg {
+    color: ${COLORS["orange"]};
+  }
+
+  & a {
+    color: currentColor;
+  }
+`;
+
+export const FooterRowSpecial = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 56px;
+
+  & ${FooterSocialList} {
+    display: none;
+  }
+
+  ${MEDIA_QUERIES["tablet"]} {
+    margin-top: 80px;
+    & ${FooterSocialList} {
+      display: flex;
+    }
+  }
+
+  ${MEDIA_QUERIES["mobile"]} {
+    margin-top: 48px;
+    flex-direction: column;
+    align-items: center;
+    gap: 48px;
+  }
+`;
+
+export const Line = styled.hr`
+  background-color: ${COLORS["orange"]};
+  width: 101px;
+  height: 4px;
+  position: absolute;
+  top: 0;
+  left: 25px;
+  border: none;
+
+  ${MEDIA_QUERIES["tablet"]} {
+    left: 40px;
+  }
+
+  ${MEDIA_QUERIES["mobile"]} {
+    left: 50%;
+    transform: translate(-50%);
+  }
 `;
